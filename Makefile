925b54691426e20e61ee45b14cb243f67428eabe
enzo <enzo.granana@ynov.com>
2026-02-18 16:51:56 +0100

    Add install script + makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	bash src/beta/install.sh
+start:
+	sudo npx tsx vlab.ts
---
+++ src/beta/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION=v0.1
+set -e
+
+echo Installing VLab beta $VERSION
+touch data/history.txt
+npm i
